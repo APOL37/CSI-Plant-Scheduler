8f23ca36e4c4c5a21a22de0f50035739ee59a970
APOL37 <acjabines.primary@gmail.com>
2026-05-19 14:06:12 +0800

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Replace "rmc-plant-scheduler" below with your actual GitHub repo name
-// e.g. if your repo URL is github.com/yourname/my-plant-app  →  base: "/my-plant-app/"
-export default defineConfig({
-  plugins: [react()],
-  base: '/CSI-Plant-Scheduler/',
-})
