be97fcc2378bcf78b81c5b2cf3cf60c74e6a6d91
APOL37 <acjabines.primary@gmail.com>
2026-05-19 13:57:29 +0800

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// Replace "rmc-plant-scheduler" below with your actual GitHub repo name
+// e.g. if your repo URL is github.com/yourname/my-plant-app  →  base: "/my-plant-app/"
+export default defineConfig({
+  plugins: [react()],
+  base: '/rmc-plant-scheduler/',
+})
